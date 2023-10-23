f7f19656cb401b8f07567bfa46563eb009485215
TahaAissat <tahayacineaissat@gmail.com>
2023-10-23 12:38:25 +0200

    update home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,8 +13,16 @@ import {
 export default function HomeScreen({ navigation }) {
   const ref_input2 = useRef();
   const ref_input3 = useRef();
+  const [username,setUserName] = useState(null)
+  const [password,setPassword] = useState(null)
+  const BACKEND_ADRESS = 'http://10.3.0.43:3000'
 
   const handleSubmit = () => {
+    fetch(`${BACKEND_ADRESS}/users/signin`, {
+      method:'POST',
+      headers:{'Content-type' : 'application/json'},
+      body : JSON.stringify({username:username,password:password})
+    })
     navigation.navigate('TabNavigator');
   };
 
@@ -23,8 +31,8 @@ export default function HomeScreen({ navigation }) {
       <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
         <View style={styles.view}>
         <Text style={styles.txt}> Veuillez compléter tous les champs pour continuer </Text>
-          <TextInput placeholder="Adresse mail" style={styles.input} autoFocus={true} placeholderTextColor={'white'} returnKeyType = {"next"} onSubmitEditing={() => ref_input2.current.focus()}/>
-          <TextInput placeholder="Mot de passe" style={styles.input} placeholderTextColor={'white'} onSubmitEditing={() => ref_input3.current.focus()} ref={ref_input2}/>
+          <TextInput onChangeText={(value) => setUserName(value)} placeholder="Username" style={styles.input} autoFocus={true} placeholderTextColor={'white'} returnKeyType = {"next"} onSubmitEditing={() => ref_input2.current.focus()}/>
+          <TextInput onChangeText={(value) => setPassword(value)} placeholder="Mot de passe" style={styles.input} placeholderTextColor={'white'} onSubmitEditing={() => ref_input3.current.focus()} ref={ref_input2}/>
           <TouchableOpacity style={styles.btn} onPress={() => handleSubmit()}>
             <Text style={styles.btntxt}> Valider</Text>
           </TouchableOpacity>

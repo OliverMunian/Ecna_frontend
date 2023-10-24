48cf081d99a718ae8052ac0d029d0a3315524bce
TahaAissat <tahayacineaissat@gmail.com>
2023-10-24 17:15:20 +0200

    rajout fiche ajout vehicule

--- screens/AddVehiculesScreen.js
+++ screens/AddVehiculesScreen.js
@@ -1,8 +1,8 @@
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import FicheAddVehicule from "../components/Fiche_AddVehicule";
 
 export default function AddVehiculesScreen () {
     return (
-        <>
-        </>
+        <FicheAddVehicule/>
     )
 }

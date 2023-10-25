b770416c61d0fd5e2d5a647495cc027290575c72
Alexandru <x238785@gmail.com>
2023-10-25 09:15:16 +0200

    frontend

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -23,7 +23,7 @@ export default function HomeScreen({ navigation }) {
   const [password,setPassword] = useState(null)
   const [errorMessage,setErrorMessage] = useState(null)
   const user = useSelector((state) => state.user.value)
-  const BACKEND_ADRESS = 'http://10.3.0.13:3000'
+  const BACKEND_ADRESS = 'http://192.168.1.17:3000'
 
   
   const handleSubmit = () => {
--- screens/InterventionsScreen.js
+++ screens/InterventionsScreen.js
@@ -1,58 +1,64 @@
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity  } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import { useEffect, useState } from 'react';
+import { useDispatch } from "react-redux";
 import Fiche_intervention from '../components/Fiche_intervention';
+import { addinterventionsToStore } from '../reducers/vehicules';
 
 export default function InterventionsScreen() {
   const [interventions, setInerventions] = useState([]);
-    const BACKEND_ADRESS = 'http://10.3.0.13:3000';
+  const dispatch = useDispatch()
+  const BACKEND_ADRESS = 'http://192.168.1.17:3000';
 
-    useEffect(() => {
-        fetch(`${BACKEND_ADRESS}/interventions/find`)
-            .then(response => response.json())
-            .then(allInterventions => {
-                setInerventions(allInterventions.Intervention);
-            });
-    }, []);
-    const handleInter = () =>{
-        console.log("test")
+  useEffect(() => {
+    fetch(`${BACKEND_ADRESS}/interventions/find`)
+      .then(response => response.json())
+      .then(allInterventions => {
+        setInerventions(allInterventions.Intervention);
+
+      });
+  }, []);
+
+  const intervention = interventions.map((inter, i) => {
+    const day = new Date(inter.date).getDate();
+    const month = new Date(inter.date).getMonth();
+    const year = new Date(inter.date).getFullYear();
+    let date = month + "/" + day + "/" + year;
+    if (inter.vehicule) {
+      dispatch(addinterventionsToStore(inter.vehicule, inter.vehicule.plaque))
+      plaque = inter.vehicule.plaque
     }
-    const intervention = interventions.map((inter, i) => {
-        const day = new Date(inter.date).getDate();
-        const month = new Date(inter.date).getMonth();
-        const year = new Date(inter.date).getFullYear();
-        let date = month + "/" + day + "/" + year;
-        return <Fiche_intervention key={i} lastName={inter.lastName} firstName={inter.firstName} 
-        departure={inter.departure} arrival={inter.arrival} date={date} 
-        plaque={inter.plaque} vehicule={inter.vehicule}/>
-        })
-        return (
-          <View style={styles.container}>
-              <Text style={styles.title}>Interventions</Text>
-              <View style={styles.line} />
-              <ScrollView
-                  horizontal={false}
-                  showsHorizontalScrollIndicator={false}
-                  endFillColor="#000"
-                  overScrollMode="never">
-                  {intervention}
-              </ScrollView>
-          </View>
-      );
+    return <Fiche_intervention key={i} lastName={inter.patient.lastName} firstName={inter.patient.firstName}
+      departure={inter.departure} arrival={inter.arrival} date={date}
+      plaque={plaque} vehicule={inter.vehicule} />
+  })
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Interventions</Text>
+      <View style={styles.line} />
+      <ScrollView
+        horizontal={false}
+        showsHorizontalScrollIndicator={false}
+        endFillColor="#000"
+        overScrollMode="never">
+        {intervention}
+      </ScrollView>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'black',
+    backgroundColor: 'black',
   },
-  title:{
-    color:'white',
+  title: {
+    color: 'white',
     fontSize: 40,
     marginTop: 60,
     marginLeft: 10,
     fontWeight: "bold",
-},
-  line:{
+  },
+  line: {
     borderBottomColor: 'grey',
     borderBottomWidth: StyleSheet.hairlineWidth,
-},
+  },
 });
